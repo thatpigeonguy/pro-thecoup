789f2ce06256e337ee82b076c7ecfac022271f42
Jonah Silvia <jonah@thecoup.xyz>
2025-07-08 19:19:27 -0400

    fixed link bug

--- components/WhyMeSection.vue
+++ components/WhyMeSection.vue
@@ -3,7 +3,8 @@
         <div class="max-w-3xl mx-auto text-center">
             <h2 class="font-bold text-4xl text-center mb-10 text-accent">Why Work With Me?</h2>
             <p class="mb-4">As a self-taught system administrator with real-world freelance experience, I bring a practical, hands-on approach to every project. I don't just follow a script; I understand the technology from the ground up. My focus is on creating secure, reliable, and easy-to-manage solutions.</p>
-            <p class="mb-4"><a href="https://thecoup.xyz" target="_blank">thecoup.xyz</a>.</p>
+            <p class="mb-4">You can view my personal projects and technical blog on my main site,
+            <a href="https://thecoup.xyz" target="_blank" class="text-accent">thecoup.xyz</a>.</p>
         </div>
     </div>
 </template>
