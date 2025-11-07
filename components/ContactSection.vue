884c04f70f17b66679a1305b24e4458d37511243
thatpigeonguy <abuttifulpigeon@proton.me>
2025-11-07 13:50:11 -0500

    removed unnecessary components

--- components/ContactSection.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="max-w-xl mx-auto text-center">
-        <h2 class="font-bold text-4xl text-center mb-10 text-accent">Ready to Start a Project?</h2>
-        <p class="mb-4">Let's talk about your needs. Reach out for a free, no-obligation consultation.</p>
-        <a href="mailto:business@thecoup.xyz" class="p-6 bg-accent text-background-dark inline-block py-3 px-6 rounded-md font-bold text-xl transition-transform duration-200 ease-in-out hover:scale-105 hover:opacity-100">business@thecoup.xyz</a>
-    </div>
-</template>
